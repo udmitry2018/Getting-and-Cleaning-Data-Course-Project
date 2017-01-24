b2428634a0759a62152fdd543ca794e1265e9fd9
Dmitry Usik <udmitry@gmail.com>
2017-01-24 07:59:21 +0300

    44

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,45 @@
-acc_x <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt', nmax = 10)
-acc_y <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt', nmax = 10)
-acc_z <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt', nmax = 10)
+#load the tables from files
 
-gyro_x <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt', nmax = 10)
-gyro_y <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt', nmax = 10)
-gyro_z <- scan('./data/run_analysis/UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt', nmax = 10)
+trainDataX<-read.table("./data/run_analysis/UCI HAR Dataset/train/X_train.txt")
+subjectTrain<-read.table("./data/run_analysis/UCI HAR Dataset/train/subject_train.txt")
+trainDataY<-read.table("./data/run_analysis/UCI HAR Dataset/train/Y_train.txt")
 
-df1 <- data.frame(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z)
+testDataX<-read.table("./data/run_analysis/UCI HAR Dataset/test/X_test.txt")
+subjectTest<-read.table("./data/run_analysis/UCI HAR Dataset/test/subject_test.txt")
+testDataY<-read.table("./data/run_analysis/UCI HAR Dataset/test/Y_test.txt")
 
-acc_x <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt', nmax = 10)
-acc_y <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt', nmax = 10)
-acc_z <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt', nmax = 10)
+features<-read.table('./data/run_analysis/UCI HAR Dataset/features.txt')
+actLabels<-read.table('./data/run_analysis/UCI HAR Dataset/activity_labels.txt')
 
-gyro_x <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt', nmax = 10)
-gyro_y <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt', nmax = 10)
-gyro_z <- scan('./data/run_analysis/UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt', nmax = 10)
-
-df2 <- data.frame(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z)
-
-df <- rbind(df1, df2)
-
-head(df, 10)
-str(df)
-typeof(df)
-
-sapply(df, sd)
-sapply(df, mean)
+#Merging the training and the test data sets
+mDataX<-rbind(trainDataX,testDataX)
+mDataY<-rbind(trainDataY,testDataY)
+mSubjects<-rbind(subjectTrain,subjectTest)
 
 
-#write.table(mydf, "./data/run_analysis/result.txt",  row.name=FALSE)
+#features ended up with the second col having the var name and the feature name, 
+FeatureNames<-features[[2]]
+
+#Uses descriptive activity names to name the activities in the data set
+names(mDataX)<-FeatureNames
+names(mDataY)<-c("ActId")
+names(mSubjects)<-c("Subject")
+#will want to join these to merged data by ActId
+names(actLabels)<-c("ActId","ActName")
+
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+relevantFeatureNames<-grep("mean|std",FeatureNames)
+relevantMData<-mDataX[,relevantFeatureNames]
+
+#labels the data set with descriptive variable names.
+mFinalActs<-merge(actLabels,mDataY,"ActId")
+relevantMData$Activity<-mFinalActs$ActName
+relevantMData$Subject<-mSubjects$Subject
+
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#the final two cols are act and subject
+finalColToAgg<-dim(relevantMData)[2]-2
+res<-aggregate(relevantMData[,1:finalColToAgg],list(act = relevantMData$Activity, subJ=relevantMData$Subject),mean, na.rm=TRUE)
+
+
+write.table(res,"./data/run_analysis/result.txt",row.name = FALSE)
